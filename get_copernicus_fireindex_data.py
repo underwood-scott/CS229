31eafbfe57be8d464086a91bdb8cd0b0fc4f8553
Scott Underwood <scott.underwood2425@gmail.com>
2023-11-27 11:26:30 -0500

    added all years of fire index data

---
+++ get_copernicus_fireindex_data.py
@@ -0,0 +1,122 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+# loop through all years
+for year in range(1994,2020,1):
+    # retrieve grib file for year of interest
+    c.retrieve(
+        'cems-fire-seasonal',
+        {
+            'format': 'grib',
+            'variable': [
+                'burning_index', 'energy_release_component', 'ignition_component',
+                'spread_component',
+            ],
+            'year': str(year),
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'day': '01',
+            'leadtime_hour': [
+                '12', '36', '60',
+                '84', '108', '132',
+                '156', '180', '204',
+                '228', '252', '276',
+                '300', '324', '348',
+                '372', '396', '420',
+                '444', '468', '492',
+                '516', '540', '564',
+                '588', '612', '636',
+                '660', '684', '708',
+                '732',
+            ],
+            'area': [
+                67.1, -165.8, 17.9,
+                -65.4,
+            ],
+            'release_version': '5',
+        },
+        'data/{}_us_data.grib'.format(str(year)))
+    
+# loop through all years
+for year in range(1994,2020,1):
+    # retrieve grib file for year of interest
+    c.retrieve(
+        'cems-fire-seasonal',
+        {
+            'format': 'grib',
+            'variable': [
+                'build_up_index', 'duff_moisture_code', 'fire_daily_severity_rating',
+                'initial_fire_spread_index',
+            ],
+            'year': str(year),
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'day': '01',
+            'leadtime_hour': [
+                '12', '36', '60',
+                '84', '108', '132',
+                '156', '180', '204',
+                '228', '252', '276',
+                '300', '324', '348',
+                '372', '396', '420',
+                '444', '468', '492',
+                '516', '540', '564',
+                '588', '612', '636',
+                '660', '684', '708',
+                '732',
+            ],
+            'area': [
+                67.1, -165.8, 17.9,
+                -65.4,
+            ],
+            'release_version': '5',
+        },
+        'data/{}_ca1_data.grib'.format(str(year)))
+    
+# loop through all years
+for year in range(1994,2020,1):
+    # retrieve grib file for year of interest
+    c.retrieve(
+        'cems-fire-seasonal',
+        {
+            'format': 'grib',
+            'variable': [
+                'drought_code', 'fine_fuel_moisture_code', 'fire_weather_index'
+            ],
+            'year': str(year),
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'day': '01',
+            'leadtime_hour': [
+                '12', '36', '60',
+                '84', '108', '132',
+                '156', '180', '204',
+                '228', '252', '276',
+                '300', '324', '348',
+                '372', '396', '420',
+                '444', '468', '492',
+                '516', '540', '564',
+                '588', '612', '636',
+                '660', '684', '708',
+                '732',
+            ],
+            'area': [
+                67.1, -165.8, 17.9,
+                -65.4,
+            ],
+            'release_version': '5',
+        },
+        'data/{}_ca2_data.grib'.format(str(year)))
